29b7a5197c366466e7e321afe0bbe662234678a8
OwnStyleFF <116486462+OwnStyleFF@users.noreply.github.com>
2025-09-13 01:15:03 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+async function enviarPregunta() {
+    const pregunta = document.getElementById("pregunta").value;
+    const respuestaDiv = document.getElementById("chat");
+
+    const resp = await fetch("https://https://backend-render-fn6z.onrender.com/api/chat", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ pregunta })
+    });
+    const data = await resp.json();
+    respuestaDiv.innerHTML += `<p class="user"><b>Tú:</b> ${pregunta}</p>`;
+    respuestaDiv.innerHTML += `<p class="ia"><b>GC AI:</b> ${data.respuesta}</p>`;
+    document.getElementById("pregunta").value = "";
+}
